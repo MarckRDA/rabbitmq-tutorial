6ecc5e3ef392e88d994071c3240ec4081eeb0175
Marcos <Marcos.RochaDaviAlves@br.experian.com>
2023-02-01 21:05:22 -0300

    up hello-world

---
+++ hello-world/send.go
@@ -0,0 +1,44 @@
+package helloworld
+
+import (
+	"context"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func Send() {
+	conn, err := amqp.Dial("amqp://myuser:mypassword@localhost:5672/")
+	FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	FailOnError(err, "Failed to declare a queue")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	FailOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
